25d7da7eb29d50059d3b94904a39642b27b10cd7
w8s8y8 <w8s8y8@126.com>
2025-05-16 10:52:12 +0800

    ADD MPLS package

---
+++ art-mpls2025.py
@@ -0,0 +1,59 @@
+import os
+import re
+import subprocess
+import shutil
+
+
+def make_iso(NewFolderName, NewISO, NewVolume):
+    subprocess.Popen(f'C:\\Program Files (x86)\\UltraISO\\UltraISO.exe -imax -output ".\\{NewISO}" -file ".\\{NewFolderName}" -volume "{NewVolume}" -joliet -jlong -lowercase').communicate()
+    shutil.copyfile(f'{NewFolderName}/cover.jpg', f'{NewFolderName}.jpg')
+    shutil.rmtree(NewFolderName)
+
+
+if __name__ == '__main__':
+    for file_name in os.listdir('.'):
+        if os.path.isdir(file_name):
+
+            ns = file_name.split(' - ')
+
+            unit = ns[1].upper()
+            date = ns[0][0:10]
+            model = ns[0][11:].upper()
+
+            mid = 'MPLStudios-%s%s%s-0' % (model[0], unit[0], unit[-1])
+
+            size = 0
+            count = 0
+
+            os.chdir(file_name)
+            for pic_name in os.listdir('.'):
+                size += os.path.getsize(pic_name)
+                if re.search('cover', pic_name):
+                    os.rename(pic_name, 'cover.jpg')
+                else:
+                    result = re.search('[0-9][0-9][0-9].jpg', pic_name)
+                    os.rename(pic_name, mid + result.group())
+                    count = count + 1
+            os.chdir('..')
+
+            size = size / (1024 * 1024)
+            if size < 1024:
+                size = int(size)
+                size = f'{size}MB'
+            else:
+                size = size / 1024
+                size = round(size, 2)
+                size = f'{size}GB'
+
+            NewFolderName = f'{model} {unit} [{count}P] HIRES ({size}) {date}'
+            NewISO = f'{model} {unit} [{count}P] ({size})'
+            NewVolume = f'{model} [{count}P]'
+
+            print(NewFolderName)
+            print(NewISO)
+            print(NewVolume)
+            print('\n')
+
+            os.rename(file_name, NewFolderName)
+
+            make_iso(NewFolderName, NewISO, NewVolume)
